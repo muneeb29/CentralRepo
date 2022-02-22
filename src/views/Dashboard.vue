1c09e2f706dc4ec7b4d8f2f3d6208ef5a1922fb9
muneeb29 <muneeb200129@gmail.com>
2022-02-22 17:22:43 +0000

    Added Sample Line Chart to Dashboard

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -2,13 +2,17 @@
   <div>
     <h1>Welcome To The User Dashboard</h1>
 
+    <div class="py-5 center" >
+      <line-chart class="border border-2 bg-light"></line-chart>
+    </div>
+
     <div class="container bg-light border">
       <add-data></add-data>
       <br />
       <br />
     </div>
 
-<div class="container py-5 hi-10"></div>
+    <div class="container py-5 hi-10"></div>
     <div class="container bg-light border">
       <search> </search>
     </div>
@@ -16,11 +20,18 @@
 </template>
 
 <script>
+import LineChart from "@/graphs/LineChart";
 import AddData from "./AddData.vue";
 import Search from "./Search.vue";
 export default {
-  components: { AddData, Search },
+  components: { LineChart, AddData, Search },
 };
 </script>
 
-<style></style>
+<style scoped>
+.center {
+  margin: 0 auto;
+  display: flex;
+  justify-content: center;
+}
+</style>
