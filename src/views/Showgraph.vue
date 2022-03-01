4c5709472204151d6666acddf8ddcc4b0c379e44
muneeb29 <98808077+muneeb29@users.noreply.github.com>
2022-03-01 11:19:49 +0000

    Added Dropdown Sections to Graphs Page

--- src/views/Showgraph.vue
+++ src/views/Showgraph.vue
@@ -4,6 +4,44 @@
         <h1>Show Graph</h1>
       </div>
 
+      <div class="container">
+        <div class="row py-3">
+          <div class="col">
+            <h4>Forms Of Cardiomyopathy</h4>
+          </div>
+            <div class="col">
+            <h4>Mutated Gene Types</h4>
+          </div>
+            <div class="col">
+            <h4>X-Axis Of Experimental Data</h4>
+          </div>
+            <div class="col">
+            <h4>Y-Axis Of Experimental Data</h4>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col">
+            <select name="" id="">
+              <option value=""></option>
+            </select>
+          </div>
+          <div class="col">
+            <select name="" id="">
+              <option value=""></option>
+            </select>
+          </div>
+          <div class="col">
+            <select name="" id="">
+              <option value=""></option>
+            </select>
+          </div>
+          <div class="col">
+            <select name="" id="">
+              <option value=""></option>
+            </select>
+          </div>
+        </div>
+      </div>
 
    <div class="py-5 center">
       <line-chart class="border border-2 bg-light"></line-chart>
